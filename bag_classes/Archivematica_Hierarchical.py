7fef822e47537dc0a0381ebace72b48dbaad76ca
Graham Hukill <ghukill@gmail.com>
2016-04-28 17:57:34 +0000

    updates to AM_hier

--- bag_classes/Archivematica_Hierarchical.py
+++ bag_classes/Archivematica_Hierarchical.py
@@ -6,32 +6,33 @@ from lxml import etree
 import mimetypes
 import metsrw
 
+from WSUDOR_Manager import utilities
+
 
 # define required `BagClass` class
 class BagClass(object):
-	
 		
 	# class is expecting a healthy amount of input from `ingestWorkspace` script, and object row
 	def __init__(self, object_row, ObjMeta, bag_root_dir, files_location, purge_bags):
 
 		# hardcoded
 		self.name = 'Archivematica_Hierarchical'  # human readable name, ideally matching filename, for this bag creating class
-		self.content_type = 'WSUDOR_HierarchicalFiles'  # not required, but easy place to set the WSUDOR_ContentType
+		self.content_type = False  
 
 		# passed
 		self.object_row = object_row  # handle for object mysql row in 'ingest_workspace_object'
 		self.ObjMeta = ObjMeta  # ObjMeta class from ouroboros.models
 		self.bag_root_dir = bag_root_dir  # path for depositing formed bags
 		self.files_location = files_location  # location of files: they might be flat, nested, grouped, etc.
-		
+		self.purge_bags = purge_bags
+
 		# derived from object_row
 		self.MODS = object_row.MODS  # MODS as XML string		
 		self.struct_map = object_row.struct_map  # JSON representation of structMap section from METS file for this object
 		self.object_title = object_row.object_title
-		self.DMDID = object_row.DMDID  # object DMDID from METS, probabl identifier for file (but not required, might be in MODS)
+		self.DMDID = object_row.DMDID  
 		self.collection_identifier = object_row.job.collection_identifier  # collection signifier, likely suffix to 'wayne:collection[THIS]'
-		
-		self.purge_bags = purge_bags
+		self.object_type = object_row.object_type
 
 		# derived
 		# MODS_handle (parsed with etree)
@@ -55,6 +56,162 @@ class BagClass(object):
 			os.mkdir("/".join([self.obj_dir,"datastreams"]))		
 
 
+	# method to create bag
+	def createBag(self):
+
+		# set identifier with filename		
+		self.full_identifier = self.collection_identifier+self.object_title.replace(".","_")
+
+		# generate PID
+		self.pid = "wayne:%s" % (self.full_identifier)
+
+		# write MODS
+		with open("%s/MODS.xml" % (self.obj_dir), "w") as fhand:
+			fhand.write(self.MODS)
+
+		# instantiate object with quick variables
+		objMeta_primer = {
+			"id": self.pid,
+			"identifier": self.full_identifier,
+			"label": self.object_title			
+		}
+
+		# Instantiate ObjMeta object
+		self.objMeta_handle = self.ObjMeta(**objMeta_primer)
+
+
+		################################################################################################################
+		
+		# set namespaces
+		ns = {
+			'mets':'http://www.loc.gov/METS/',
+			'premis':'info:lc/xmlns/premis-v2',
+			'fits':'http://hul.harvard.edu/ois/xml/ns/fits/fits_output'
+		}		
+
+		# open mets		
+		temp_filename = '/tmp/Ouroboros/%s.xml' % uuid.uuid4()
+		with open(temp_filename, 'w') as fhand:
+			fhand.write(self.object_row.job.ingest_metadata)
+		mets = metsrw.METSDocument.fromfile(temp_filename)
+		os.remove(temp_filename)
+
+		# Build datstream dictionary
+		fs = fsByLabel(mets,self.object_title)
+
+		# determine parent
+		try:
+			parent_label = fs.parent.label.replace(".","_")
+			parent_pid = "wayne:%s%s" % (self.collection_identifier, parent_label)
+		except:
+			print "Parent not found, setting collection PID"
+			parent_pid = "wayne:collection%s" % (self.collection_identifier)
+
+		# write parent
+		self.objMeta_handle.object_relationships.append({
+			"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasParent",
+			"object": parent_pid
+		})
+
+		
+		# for "Directory" types
+		'''
+		Has much less information than files, but does include parent
+		'''
+		if self.object_type == "Directory":
+			
+			# set content types
+			self.content_type = "WSUDOR_Container"
+			self.objMeta_handle.content_type = self.content_type
+
+			# set isRepresentedBy
+			self.objMeta_handle.isRepresentedBy = False
+
+		# for "Item" types
+		'''
+		With binary files, comes more information.  We can derive mimetype, and thus ContentType, from the fits metadata"
+		'''
+		if self.object_type == "Item":
+
+			# determine mimetype
+
+			# APPROACH #1 : use Archivematica analysis of file
+			try:
+				print "Attempting to derive mimetype from Archivematica output"
+				# get amd section
+				amd = fs.amdsecs[0]
+
+				# get techMD as lxml element
+				for sub in amd.subsections:
+					if sub.id_string().startswith('techMD'):
+						techMD = sub.serialize()
+
+				# get identity section from fits tool, via premis event
+				fits_identity = techMD.xpath('//premis:objectCharacteristicsExtension/fits:fits/fits:identification/fits:identity',namespaces=ns)[0]
+
+				# get mimetype
+				derived_mime_type = fits_identity.attrib['mimetype']
+			
+			# APPROACH #2 : fall back on file extension			
+			except:
+				print "Could not determine mime type of file from Archivematica output, attempting file extension"
+
+				# get file extension
+				file_ext = fs.path.split(".")[-1]
+
+				# use mimetypes library
+				mimetypes.init()
+				derived_mime_type = mimetypes.types_map[".%s" % file_ext]
+
+			# Set content type here
+			if derived_mime_type in utilities.mime_CM_hash:
+				self.content_type = utilities.mime_CM_hash[derived_mime_type]
+				self.objMeta_handle.content_type = self.content_type
+			else:
+				print "could not determine mimetype from fits, skipping"
+				return False
+
+			# write datastream
+			self._makeDatastream(fs)
+
+		################################################################################################################
+
+
+		# write known relationships
+		add_rels = [
+			{
+				"predicate": "info:fedora/fedora-system:def/relations-external#isMemberOfCollection",
+				"object": "info:fedora/wayne:collection%s" % (self.collection_identifier)
+			},
+			{
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isDiscoverable",
+				"object": "info:fedora/True"
+			},
+			{
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/preferredContentModel",
+				"object": "info:fedora/CM:%s" % (self.content_type.split("_")[1])
+			},
+			{
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasSecurityPolicy",
+				"object": "info:fedora/wayne:WSUDORSecurity-permit-apia-unrestricted"
+			}
+		]
+		for rel in add_rels:
+			self.objMeta_handle.object_relationships.append(rel)
+
+		# write to objMeta.json file
+		self.objMeta_handle.writeToFile("%s/objMeta.json" % (self.obj_dir))
+
+		# make bag
+		bagit.make_bag(self.obj_dir, {
+			'Collection PID': "wayne:collection"+self.collection_identifier,
+			'Object PID': self.pid
+		}, processes=1)
+
+		return self.obj_dir
+
+
+	# helper function to build datastream directory
 	def _makeDatastream(self, fs):
 
 		# Identify datastreams folder
@@ -70,6 +227,10 @@ class BagClass(object):
 		# get extension, ds_id
 		mimetypes.init()
 		ds_id, ext = os.path.splitext(filename)
+
+		# fix ds_id and label
+		ds_id = "DS%s" % ds_id
+		label = "DS%s" % label
 
 		# create datastream dictionary
 		ds_dict = {
@@ -94,83 +255,12 @@ class BagClass(object):
 		if int(order) == 1:
 			self.objMeta_handle.isRepresentedBy = ds_id
 
-	def createBag(self):
 
-		# set identifier
-		self.full_identifier = self.collection_identifier+self.DMDID
-		print self.full_identifier
-
-		# generate PID
-		self.pid = "wayne:%s" % (self.full_identifier)
-
-		# write MODS
-		with open("%s/MODS.xml" % (self.obj_dir), "w") as fhand:
-			fhand.write(self.MODS)
-
-		# instantiate object with quick variables
-		objMeta_primer = {
-			"id": self.pid,
-			"identifier": self.full_identifier,
-			"label": self.object_title,
-			"content_type": self.content_type
-		}
-
-		# Instantiate ObjMeta object
-		self.objMeta_handle = self.ObjMeta(**objMeta_primer)
-
-		# open mets
-		'''
-		Obviously, want to avoid this decode / encode thing here...
-		'''
-		mets = metsrw.METSDocument.fromtree( etree.fromstring(self.object_row.job.ingest_metadata.decode('utf-8').encode('ascii')) )
-		print mets.all_files()
-
-		# Build datstream dictionary
-		fs = mets.get_file(self.DMDID)
-		'''
-		at this point, we can know the mimetype of this file, and trigger the correct WSUDOR_ContentType
-			- assumes object is "simple", only one file (maybe the best way... look at converting others)
-			- trigger CM based on mimetype as determined by Archivematica
-
-		for "container" / directory front-end view, we'll see the thumbnails of children objects
-		'''
-
-		self._makeDatastream(fs)
-
-		# write known relationships
-		self.objMeta_handle.object_relationships = [
-			{
-				"predicate": "info:fedora/fedora-system:def/relations-external#isMemberOfCollection",
-				"object": "info:fedora/wayne:collection%s" % (self.collection_identifier)
-			},
-			{
-				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isDiscoverable",
-				"object": "info:fedora/True"
-			},
-			{
-				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/preferredContentModel",
-				"object": "info:fedora/CM:%s" % (self.content_type.split("_")[1])
-			},
-			{
-				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasSecurityPolicy",
-				"object": "info:fedora/wayne:WSUDORSecurity-permit-apia-unrestricted"
-			}
-		]
-
-		# write to objMeta.json file
-		self.objMeta_handle.writeToFile("%s/objMeta.json" % (self.obj_dir))
-
-		# make bag
-		bagit.make_bag(self.obj_dir, {
-			'Collection PID': "wayne:collection"+self.collection_identifier,
-			'Object PID': self.pid
-		}, processes=1)
-
-		return self.obj_dir
-
-
-
-
+# HELPER FUNCTIONS (TO MOVE)
+def fsByLabel(mets,label):
+	for fs in mets.all_files():
+		if fs.label == label:
+			return fs
 
 
 
